81ce61e15bda12ea2bb3d329e368babc3d70d8ab
Apurva12343 <apurvaaryan2222@gmail.com>
2025-03-06 00:41:08 +0530

    01:41

--- src/pipelines/training_pipelines.py
+++ src/pipelines/training_pipelines.py
@@ -9,5 +9,6 @@ if __name__=='__main__':
     obj=DataIngestion()
     train_data_path,test_data_path=obj.initiate_data_ingestion()
     print(train_data_path,test_data_path)
-
+    datatransformation = DataTransformation()
+    datatransformation.initiate_data_transformation(train_data_path,test_data_path)
      
