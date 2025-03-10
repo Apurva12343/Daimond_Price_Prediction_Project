d420c651b438e0ccd7dcfeaa4af4b580cbb882d6
Apurva12343 <apurvaaryan2222@gmail.com>
2025-03-10 23:24:24 +0530

    23:24

--- src/utils.py
+++ src/utils.py
@@ -19,3 +19,20 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+def evaluate_model(x_train,y_train,x_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            logging.info("model training started")
+            model = list(models.values())[i]
+            #Training Model
+            model.fit(x_train,y_train)
+            #Predict Testing Data
+            y_test_pred = model.predict(x_test)
+            # R2 score for train and test data
+            test_model_score = r2_score(y_test,y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+            logging.info(f"{model} trained")
+        return report
+    except Exception as e:
+        logging.info("Exception occured during model training")
