3fb825c4056cb35f7f7d1ec0f1445148fc914499
Apurva12343 <apurvaaryan2222@gmail.com>
2025-03-11 00:43:09 +0530

    12:42

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,8 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression,Ridge,Lasso,ElasticNet
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.svm import SVR
-from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor,GradientBoostingRegressor
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object
